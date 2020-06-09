abfd80b1a38bcfb6646250d4b0f5ca855f1e7489
Jake Champion <me@jakechampion.name>
2020-06-09 17:43:48 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 import oTable from './src/js/oTable';
 
 const constructAll = function() {
-	oTable.init();
-	document.removeEventListener('o.DOMContentLoaded', constructAll);
-};
-
-document.addEventListener('o.DOMContentLoaded', constructAll);
+ oTable.init();
+document.removeEventListener('o.DOMContentLoaded', constructAll)
+}document.addEventListener('o.DOMContentLoaded', constructAll)
 
 export default oTable;
