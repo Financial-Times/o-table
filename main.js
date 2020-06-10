697a08a6b26dababbe771eeb2140ddf60089c4cd
Jake Champion <me@jakechampion.name>
2020-06-10 14:01:11 +0100

    Revert main.js

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-import oTable from "./src/js/oTable";
+import oTable from './src/js/oTable';
 
 const constructAll = function() {
- oTable.init();
-document.removeEventListener('o.DOMContentLoaded', constructAll)
-};document.addEventListener('o.DOMContentLoaded', constructAll)
+	oTable.init();
+	document.removeEventListener('o.DOMContentLoaded', constructAll);
+};
+
+document.addEventListener('o.DOMContentLoaded', constructAll);
 
 export default oTable;
