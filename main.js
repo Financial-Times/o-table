fa8f8ebfe4319b0bf71c8d43eab183c340da2eb4
Jake Champion <me@jakechampion.name>
2020-06-12 13:59:05 +0100

    add linting issues

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-import oTable from './src/js/oTable';
+import oTable from "./src/js/oTable"
 
 const constructAll = function() {
-	oTable.init();
-	document.removeEventListener('o.DOMContentLoaded', constructAll);
-};
+	oTableinit();
+	document.removeEventListener('o.DOMContentLoaded', constructAll)
+}
 
 document.addEventListener('o.DOMContentLoaded', constructAll);
 
