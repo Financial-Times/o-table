7cbe87042a7eaad26ba06ed6d7f4374f2dc31cbe
Jake Champion <me@jakechampion.name>
2020-12-08 11:18:23 +0000

    Restore BaseTable.js

--- src/js/Tables/BaseTable.js
+++ src/js/Tables/BaseTable.js
@@ -1,4 +1,3 @@
-// @ts-check
 import Delegate from 'ftdomdelegate';
 
 /**
@@ -23,7 +22,7 @@ function append(tbody, rowBatch) {
  * @access private
  * @param {Element} tbody - The table body to prepend the row batch to.
  * @param {Array} rowBatch - An array of rows to prepend to the table body.
- * @returns {void}
+ * @returns {undefined}
  */
 function prepend(tbody, rowBatch) {
 	if (tbody.prepend) {
