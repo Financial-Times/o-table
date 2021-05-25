ff43e027ebde771edecf5be89969a1fa444cd92c
Jake Champion <me@jakechampion.name>
2021-05-25 10:48:21 +0100

    Add test to prove multiple filters work correctly on a single table

--- test/BaseTable.test.js
+++ test/BaseTable.test.js
@@ -98,6 +98,95 @@ describe("BaseTable", () => {
 				}, 1000); // wait for window.requestAnimationFrame
 			});
 		});
+
+		context('multiple declarative table filters', () => {
+			it('applies filter options via already existing `selected` attributes', (done) => {
+				sandbox.init();
+				sandbox.setContents(fixtures.tableWithContainerAndComplexHeadings);
+				const genus = ['Stenocereus', 'Durio'];
+				const characteristic = ['Juicy', 'Smelly'];
+				oTableEl = document.querySelector('[data-o-component=o-table]');
+				sandbox.addContents(`
+					<select
+						id="genus-filter-input"
+						data-o-table-filter-id="table-id"
+						data-o-table-filter-column="1"
+					>
+						<option value="">All</option>
+						<!-- "selected" indicates the default filter -->
+						<option value="Stenocereus" selected>Stenocereus</option>
+						<option value="Durio">Durio</option>
+					</select>
+
+					<select
+						id="characteristic-filter-input"
+						data-o-table-filter-id="table-id"
+						data-o-table-filter-column="2"
+					>
+						<option value="">All</option>
+						<!-- "selected" indicates the default filter -->
+						<option value="Juicy" selected>Juicy</option>
+						<option value="Smelly">Smelly</option>
+					</select>
+				`);
+				// Init table.
+				table = new BaseTable(oTableEl, sorter);
+				// Confirm default filter (set declaratively).
+				setTimeout(() => {
+					try {
+						assertFilter(genus, ['Stenocereus']);
+						assertFilter(characteristic, ['Juicy']);
+						done();
+					} catch(error) {
+						done(error);
+					}
+				}, 1000); // wait for window.requestAnimationFrame
+			});
+
+			it('applies filters based on user interaction', (done) => {
+				sandbox.init();
+				sandbox.setContents(fixtures.tableWithContainerAndComplexHeadings);
+				const genus = ['Stenocereus', 'Durio'];
+				const characteristic = ['Juicy', 'Smelly'];
+				oTableEl = document.querySelector('[data-o-component=o-table]');
+				sandbox.addContents(`
+					<select
+						id="genus-filter-input"
+						data-o-table-filter-id="table-id"
+						data-o-table-filter-column="1"
+					>
+						<option value="">All</option>
+						<option value="Stenocereus">Stenocereus</option>
+						<option value="Durio">Durio</option>
+					</select>
+
+					<select
+						id="characteristic-filter-input"
+						data-o-table-filter-id="table-id"
+						data-o-table-filter-column="2"
+					>
+						<option value="">All</option>
+						<option value="Juicy">Juicy</option>
+						<option value="Smelly">Smelly</option>
+					</select>
+				`);
+				// Init table.
+				table = new BaseTable(oTableEl, sorter);
+				const genusSelect = document.getElementById('genus-filter-input');
+				genusSelect.options[2].setAttribute('selected', 'selected');
+				const characteristicSelect = document.getElementById('characteristic-filter-input');
+				characteristicSelect.options[2].setAttribute('selected', 'selected');
+				setTimeout(() => {
+					try {
+						assertFilter(genus, ['Durio']);
+						assertFilter(characteristic, ['Smelly']);
+						done();
+					} catch(error) {
+						done(error);
+					}
+				}, 1000); // wait for window.requestAnimationFrame
+			});
+		});
 	});
 
 	describe('filter', () => {
